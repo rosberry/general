3e393d27e274e7e9b8df17ba4fb08d0107470942
Artem Novichkov <artem.novichkov@rosberry.com>
2020-06-09 18:28:46 +0600

    Add list command

--- Sources/General/Commands/General.swift
+++ Sources/General/Commands/General.swift
@@ -7,5 +7,5 @@ import ArgumentParser
 final class General: ParsableCommand {
 
     static let configuration: CommandConfiguration = .init(abstract: "Generates code from templates.",
-                                                           subcommands: [Generate.self, Create.self, Spec.self])
+                                                           subcommands: [Generate.self, Create.self, Spec.self, List.self])
 }
---
+++ Sources/General/Commands/List.swift
@@ -0,0 +1,59 @@
+//
+//  Copyright © 2020 Rosberry. All rights reserved.
+//
+import Foundation
+import ArgumentParser
+
+final class List: ParsableCommand {
+
+    private lazy var fileManager: FileManager = .default
+
+    // MARK: - Parameters
+
+    static let configuration: CommandConfiguration = .init(abstract: "List of available templates.")
+
+    func run() throws {
+        let url = fileManager.homeDirectoryForCurrentUser + Constants.templatesFolderName
+        try printContentOfDirectory(at: url)
+    }
+
+    // MARK: - Private
+
+    private func printContentOfDirectory(at url: URL) throws {
+        var contents = try fileManager.contentsOfDirectory(at: url,
+                                                           includingPropertiesForKeys: [.isDirectoryKey],
+                                                           options: [.skipsHiddenFiles])
+        contents.sort { lhs, rhs in
+            if lhs.lastPathComponent == Constants.commonTemplatesFolderName {
+                return true
+            }
+            if rhs.lastPathComponent == Constants.commonTemplatesFolderName {
+                return false
+            }
+            return lhs.path < rhs.path
+        }
+        try contents.forEach { url in
+            if url.lastPathComponent == Constants.commonTemplatesFolderName {
+                try printCommonTemplates(at: url)
+            }
+            else if url.hasDirectoryPath {
+                print(url.lastPathComponent)
+            }
+        }
+    }
+
+    private func printCommonTemplates(at url: URL) throws {
+        var contents = try fileManager.contentsOfDirectory(at: url,
+                                                           includingPropertiesForKeys: [.isDirectoryKey],
+                                                           options: [.skipsHiddenFiles])
+        contents = contents.filter { url in
+            url.pathExtension == "stencil"
+        }
+        if contents.count > 0 {
+            print("Common templates:")
+        }
+        for content in contents {
+            print("\t", content.lastPathComponent)
+        }
+    }
+}

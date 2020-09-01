b1e063ca9e7e7cfe7569423f176fbb2e421b2f3a
nik <nikolay.tynin@rosberry.com>
2020-09-01 17:35:44 +0600

    improve templates

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [.macOS(.v10_12)],
     dependencies: [
         //with bumped PathKit version
-        .package(url: "https://github.com/artemnovichkov/StencilSwiftKit.git", .branch("stable")),
+        .package(url: "https://github.com/rosberry/StencilSwiftKit.git", .branch("stable")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.1.0")),
         .package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "0.0.0")),
         .package(url: "https://github.com/tuist/XcodeProj.git", .upToNextMajor(from: "7.0.0"))

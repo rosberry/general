f54480eea13fbefc8b39b2d718adc2222d1dda50
ntunin <nikolay.tyunin@rosberry.com>
2021-01-22 14:59:56 +0600

    update version

--- Sources/General/Commands/General.swift
+++ Sources/General/Commands/General.swift
@@ -7,7 +7,7 @@ import ArgumentParser
 final class General: ParsableCommand {
 
     static let configuration: CommandConfiguration = .init(abstract: "Generates code from templates.",
-                                                           version: "0.2",
+                                                           version: "0.3",
                                                            subcommands: [Generate.self,
                                                                          Create.self,
                                                                          Spec.self,

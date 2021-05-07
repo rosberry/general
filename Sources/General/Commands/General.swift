5d2d1be5416e172e3e975f5b89de6f6b49e7b899
ntunin <nikolay.tyunin@rosberry.com>
2021-05-07 09:55:40 +0600

    improve rendered files alignment

--- Sources/General/Commands/General.swift
+++ Sources/General/Commands/General.swift
@@ -7,7 +7,7 @@ import ArgumentParser
 final class General: ParsableCommand {
 
     static let configuration: CommandConfiguration = .init(abstract: "Generates code from templates.",
-                                                           version: "0.3",
+                                                           version: "0.3.2",
                                                            subcommands: [Generate.self,
                                                                          Create.self,
                                                                          Spec.self,

97d89b5e9a379c36f8a5739e8a1c4c6d3d323f5b
ntunin <nikolay.tyunin@rosberry.com>
2021-05-27 09:46:36 +0600

    fix code review issues

--- Sources/GeneralKit/Services/UpgradeService/UpgradeServiceImpl.swift
+++ Sources/GeneralKit/Services/UpgradeService/UpgradeServiceImpl.swift
@@ -56,6 +56,6 @@ public final class UpgradeServiceImpl: UpgradeService {
     }
 
     private func buildGeneral() throws {
-        try dependencies.shell(loud: "cd \(Constants.downloadedSourcePath); make install")
+        try dependencies.shell(loud: "cd \(Constants.downloadedSourcePath) && make install")
     }
 }

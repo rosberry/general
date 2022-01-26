34725fd04118c33eab95e074beb8eef18259491b
Tublad <tublad92@gmail.com>
2022-01-26 14:40:33 +0600

    remove test commond

--- Sources/GeneralIOs/Commands/GeneralIOS.swift
+++ Sources/GeneralIOs/Commands/GeneralIOS.swift
@@ -6,22 +6,12 @@ import ArgumentParser
 import Foundation
 import GeneralKit
 
-// TODO: remove test command
-
-final class Test: ParsableCommand {
-    public static let configuration: CommandConfiguration = .init(commandName: "test", abstract: "Runs test command for check completions works")
-
-    func run() throws {
-        print(green("Test command run"))
-    }
-}
-
 final class GeneralIOS: ParsableCommand {
     static var configuration: CommandConfiguration {
 
         return .init(abstract: "Generates code from templates.",
                      version: "0.0.1",
-                     subcommands: [Generate.self, Setup.self, Test.self],
+                     subcommands: [Generate.self, Setup.self],
                      defaultSubcommand: Generate.self)
     }
 }

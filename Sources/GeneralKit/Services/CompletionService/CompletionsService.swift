a811a84410b5c51a6e476e6038f0e2ad3fbe174d
Tublad <tublad92@gmail.com>
2022-03-03 14:13:53 +0600

    update completion service

--- Sources/GeneralKit/Services/CompletionService/CompletionsService.swift
+++ Sources/GeneralKit/Services/CompletionService/CompletionsService.swift
@@ -9,9 +9,12 @@ public protocol HasCompletionsService {
 }
 
 public protocol CompletionsService {
+    var generateOptionName: String { get }
     func templates() -> [String]
     func installedPlugins() -> [String]
     func executables() -> [String]
     func templatesRepos() -> [String]
     func versions() -> [String]
+    func defineCompletionShell() -> CompletionShell?
+    func overrideCompletionScript(config: CompletionConfig) -> String
 }

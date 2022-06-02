27b7ff625be27096ac29549c41f0b81a46a794d1
Tublad <tublad92@gmail.com>
2022-06-02 15:26:57 +0600

    add askCompany

--- Sources/GeneralIOs/AskProject.swift
+++ Sources/GeneralIOs/AskProject.swift
@@ -8,3 +8,7 @@ public func askProject() -> String? {
     let defaultValue = try? ProjectService.findProject()?.url.lastPathComponent
     return ask("Enter project name", default: defaultValue)
 }
+
+public func askCompany() -> String? {
+    return ask("Enter company name", default: "Rosberry")
+}

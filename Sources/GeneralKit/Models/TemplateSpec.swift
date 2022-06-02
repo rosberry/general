8dbbc591d63ace869168245cdc866bc7fc2fae73
Tublad <tublad92@gmail.com>
2022-06-02 15:27:45 +0600

    add marked for path at file Services.swift

--- Sources/GeneralKit/Models/TemplateSpec.swift
+++ Sources/GeneralKit/Models/TemplateSpec.swift
@@ -6,10 +6,12 @@ import Foundation
 
 public struct TemplateSpec: Codable, CustomStringConvertible {
     public let files: [File]
+    public let marked: String?
     public let suffix: String?
 
-    public init(files: [File], suffix: String? = nil) {
+    public init(files: [File], marked: String? = nil, suffix: String? = nil) {
         self.files = files
         self.suffix = suffix
+        self.marked = marked
     }
 }
